ae89cd147a5ca318d01417bc2f36bc1c795ae3ce
Diego Katabian <140689707+DiegoKatabian@users.noreply.github.com>
2024-02-23 17:31:24 -0300

    Ajustes Feedback Ariel

---
+++ AC-Arg/Assets/Scripts/HealthComponent.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class HealthComponent : MonoBehaviour
+{
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
