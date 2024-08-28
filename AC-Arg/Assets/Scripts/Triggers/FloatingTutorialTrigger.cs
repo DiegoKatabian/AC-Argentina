7ffee3699e4eda6b87c30b975c4c41d5a63d961b
DiegoKatabian <140689707+DiegoKatabian@users.noreply.github.com>
2024-08-27 22:07:11 -0300

    floating tutorials pt 2

--- AC-Arg/Assets/Scripts/Triggers/FloatingTutorialTrigger.cs
+++ AC-Arg/Assets/Scripts/Triggers/FloatingTutorialTrigger.cs
@@ -6,12 +6,47 @@ using UnityEngine;
 public class FloatingTutorialTrigger : MonoBehaviour
 {
     [SerializeField] GameObject floatingTutorialCanvas;
+    [SerializeField] bool usesGameEventAsTurnOffCondition = false;
+    [SerializeField] Evento gameEventToTurnOff;
+    [SerializeField] bool isOneTimeOnly = false;
+    bool wasTriggered = false;
+
+
+    private void Start()
+    {
+        if (usesGameEventAsTurnOffCondition)
+        {
+            EventManager.Instance.Subscribe(gameEventToTurnOff, TurnOffTutorial);
+
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (usesGameEventAsTurnOffCondition)
+        {
+            EventManager.Instance.Unsubscribe(gameEventToTurnOff, TurnOffTutorial);
+        }
+    }
+
+    private void TurnOffTutorial(object[] parameters)
+    {
+        if (usesGameEventAsTurnOffCondition && wasTriggered)
+        {
+            floatingTutorialCanvas.SetActive(false);
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            if (isOneTimeOnly && wasTriggered)
+            {
+                return;
+            }
             floatingTutorialCanvas.SetActive(true);
+            wasTriggered = true;
         }
     }
 
